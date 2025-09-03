3f02836d8518c9aebcb4c4a917b16f022aa7b381
unknown <pabloarchuleta@gmail.com>
2025-09-02 20:34:38 -0500

    Added Frontend file structure

---
+++ frontend/src/pages/Main.tsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header";
+import Prediction from "../components/Prediction";
+import Footer from "../components/Footer";
+
+const Main: React.FC = () => {
+  return (
+    <main>
+        <Header/>
+        <Prediction/>
+        <Footer/>
+    </main>
+  )
+}
+
+export default Main;
