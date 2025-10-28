0cb32ec7a7869ab936b37503c9cbe8f34fed0662
pabloarchuleta <pablo.archuleta@lcu.edu>
2025-10-28 10:13:10 -0500

    Completed ThemeContext.tsx

--- frontend/src/context/ThemeContext.tsx
+++ frontend/src/context/ThemeContext.tsx
@@ -5,4 +5,26 @@ type ThemeContextType = {
   toggleTheme: () => void;
 }
 
-const ThemeContext = createContext<ThemeContextType | undefined>(undefined)
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+const ThemeProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const [theme, setTheme] = useState("light")
+
+  const toggleTheme = () => {
+    setTheme((prev) => (prev === "light" ? "dark" : "light"))
+  }
+
+  useEffect(() => {
+    document.body.classList.remove("theme-light", "theme-dark");
+    document.body.classList.add(`theme-${theme}`);
+  }, [theme])
+
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme}}>
+      {children}
+    </ThemeContext.Provider>
+  )
+}
+
+export { ThemeContext, ThemeProvider}
